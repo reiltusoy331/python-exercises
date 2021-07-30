6fbe0e38c5c508b821fde811b5ef4304960aca84
reiltusoy331 <reiltusoy331@gmail.com>
2021-07-30 23:58:36 +0800

    Forty-nineth batch of python files to commit.

---
+++ Exercise 144. Guess My Number App.py
@@ -0,0 +1,20 @@
+import random
+
+print('\t\t\t=== Welcome to the Guess My Number App ===')
+
+player = input('Hello! What is your name: ').title()
+print(f'Well {player}, I am thinking of a number between 1 and 20.')
+
+guess_number = random.randint(1, 20)
+
+for num in range(1, 6):
+    user_guess = int(input('\nTake a guess: '))
+    if user_guess < guess_number:
+        print('Your guess is too low.')
+    elif user_guess > guess_number:
+        print('Your guess is too high.')
+    elif user_guess == guess_number:
+        print(f'\nGood Job, {player}! You guessed my number in {num} guesses!')
+        break
+    else:
+        print(f'\nGame Over. The number I was thinking of was {guess_number}.')
