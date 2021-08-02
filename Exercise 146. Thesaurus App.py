f162536b0b70002f04ba51247079c661592e54b7
reiltusoy331 <reiltusoy331@gmail.com>
2021-08-02 20:20:47 +0800

    Fifty-first batch of python files to commit.

---
+++ Exercise 146. Thesaurus App.py
@@ -0,0 +1,9 @@
+
+print('\t\t\t=== Welcome to the Thesaurus App ===')
+
+print('Choose a word from the thesaurus and I will give you a synonym.')
+
+thesaurus = ['hot', 'cold', 'happy', 'sad']
+print('Here are the words in the thesaurus: ')
+for word in thesaurus:
+    print(f'\t-{word}')
