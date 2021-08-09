ce0048a9f31858a7b8858e2e1cdd0d5bef9d825e
reiltusoy331 <reiltusoy331@gmail.com>
2021-08-09 08:41:43 +0800

    Fifty-first batch of python files to commit.

--- Exercise 146. Thesaurus App.py
+++ Exercise 146. Thesaurus App.py
@@ -1,9 +1,35 @@
-
+import random
 print('\t\t\t=== Welcome to the Thesaurus App ===')
 
 print('Choose a word from the thesaurus and I will give you a synonym.')
 
-thesaurus = ['hot', 'cold', 'happy', 'sad']
+thesaurus = {
+    'hot': ['balmy', 'summery', 'tropical', 'boiling', 'scorching'],
+    'cold': ['chilly', 'cool', 'freezing', 'frigid', 'polar'],
+    'happy': ['content', 'cheery', 'merry', 'jovial', 'jocular'],
+    'sad': ['unhappy', 'downcast', 'miserable', 'glum', 'melancholy']
+}
+
+
 print('Here are the words in the thesaurus: ')
-for word in thesaurus:
+for word in thesaurus.keys():
     print(f'\t-{word}')
+
+user_input = input('What word would you like a synonym for: ').lower().strip()
+
+if user_input in thesaurus:
+    index = random.randint(0, 4)
+    print(f'A synonym for {user_input} is {thesaurus[user_input][index]}')
+else:
+    print('The selected word is not in the choices.')
+
+print_thesaurus = input(
+    'Would you like to see the whole thesaurus(\'yes/no\'): ').lower().strip()
+
+if print_thesaurus == 'yes':
+    for key, values in thesaurus.items():
+        print(f'\n {key.title()} synonyms are: ')
+        for value in values:
+            print(f'\t- {value}')
+else:
+    print('I hope you enjoyed this app.')
